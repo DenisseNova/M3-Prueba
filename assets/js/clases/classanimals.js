064a7c25339e3825a8050e10ed5fa175a5674008
Denisse Nova <denisse.nova.v@gmail.com>
2021-08-22 16:36:54 -0400

    class animal js

---
+++ assets/js/clases/classanimals.js
@@ -0,0 +1,33 @@
+class Animales {
+  constructor (nombre, edad, img, comentarios, sonidos){
+    let Nombre = nombre;
+    let Edad = edad;
+    let Imagenes = img;
+    let Comentarios = comentarios;
+    let Sonido = sonidos;
+
+    this.getNombre = () => Nombre;
+    this.getEdad = () => Edad;
+    this.getImg = () => Imagenes;
+    this.setComentarios = (nuevoComentarios) => Comentarios = nuevoComentarios;
+    this.getSonido = () => Sonido;
+  }
+  get Nombre(){
+    return this.getNombre();
+  }
+  get Edad(){
+    return this.getEdad();
+  }
+  get Imagenes(){
+    return this.getImg();
+  }
+  get Sonido(){
+    return this.getSonido();
+  }
+  set Comentarios (nuevoComentarios){
+    return this.setComentarios(nuevoComentarios);
+  }
+}
+console.log(new Animales("oso", "12", "..","sound"))
+
+export default Animales;
